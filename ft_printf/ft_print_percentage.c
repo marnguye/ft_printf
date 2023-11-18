60a764c7829cb4a1b0ec31cae5bcddbc8e8671f7
martin <144384507+midikismal@users.noreply.github.com>
2023-11-18 18:00:32 +0100

    Delete ft_printf directory

--- ft_printf/ft_print_percentage.c
+++
@@ -1,8 +0,0 @@
-#include "ft_printf.h"
-
-int ft_print_percentage(void)
-{
-	char percent = '%';
-	write(1, &percent, 1);
-	return (1);
-}
--- ft_printf/ft_putchar.c
+++
@@ -1,7 +0,0 @@
-#include "ft_printf.h"
-
-int ft_putchar(int c)
-{
-	write(c, &c, 1);
-	return (1);
-}
